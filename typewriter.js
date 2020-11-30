2f7768f32b258452996b8c629e0ee6d919de7e44
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-11-30 03:00:47 +0000

    added animation to the string using setTimeout

---
+++ typewriter.js
@@ -0,0 +1,7 @@
+const sentence = "hello there from lighthouse labs";
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(( ) => process.stdout.write(sentence[i]),i*50);
+  if (i === sentence.length - 1) {
+   setTimeout(() => process.stdout.write('\n'),sentence.length*50);
+  }
+}
